c91bf2b7c0d32dc64715c62dfde8a90e3012d3f7
Marion PERRET <marionprrt@gmail.com>
2019-12-09 16:25:10 +0100

    :construction: Integration PlayerAudioManager

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+public class AudioManager : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    public string Ambiance_BaseEvent;
+    [FMODUnity.EventRef]
+    public string Ambiance_Round2Event;
+    [FMODUnity.EventRef]
+    public string Ambiance_Round3Event;
+    [FMODUnity.EventRef]
+    public string Télé_allumée_backgroundEvent;
+    [FMODUnity.EventRef]
+    public string Télé_glitchEvent;
+    [FMODUnity.EventRef]
+    public string Télé_switchOFFEvent;
+    [FMODUnity.EventRef]
+    public string TonerreEvent;
+    [FMODUnity.EventRef]
+    public string Voiture_LtoREvent;
+    [FMODUnity.EventRef]
+    public string Voiture_Police_LtoREvent;
+    [FMODUnity.EventRef]
+    public string Voiture_RtoLEvent;
+    [FMODUnity.EventRef]
+    public string Voiture_Police_RtoLEvent;
+
+    
+}
---
+++ Assets/Scripts/Audio/PlayerAudioManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+public class PlayerAudioManager : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    public string dashEvent;
+    [FMODUnity.EventRef]
+    public string attaque1_etape1_wooshEvent;
+    [FMODUnity.EventRef]
+    public string attaque2_etape1_wooshEvent;
+    [FMODUnity.EventRef]
+    public string block_armesEvent;
+    [FMODUnity.EventRef]
+    public string Hit_bodyEvent;
+    [FMODUnity.EventRef]
+    public string Epee_out_fourreauEvent;
+      [FMODUnity.EventRef]
+    public string Epee_in_fourreauEvent;
+     [FMODUnity.EventRef]
+    public string FootstepsEvent;
+    
+
+    public void AudioDash()
+    {
+        RuntimeManager.PlayOneShot(dashEvent);
+    }
+
+    public void AudioAttaque1_Woosh()
+    {
+        RuntimeManager.PlayOneShot(attaque1_etape1_wooshEvent);
+    }
+    public void AudioAttaque2_Woosh()
+    {
+        RuntimeManager.PlayOneShot(attaque2_etape1_wooshEvent);
+    }
+    public void AudioBlockArmes()
+    {
+        RuntimeManager.PlayOneShot(block_armesEvent);
+    }
+    public void AudioHitBody()
+    {
+        RuntimeManager.PlayOneShot(Hit_bodyEvent);
+    }
+    public void AudioEpee_Out()
+    {
+        RuntimeManager.PlayOneShot(Epee_out_fourreauEvent);
+    }
+    public void AudioEpee_in()
+    {
+        RuntimeManager.PlayOneShot(Epee_in_fourreauEvent);
+    }
+    public void AudioFootsteps()
+    {
+        RuntimeManager.PlayOneShot(FootstepsEvent);
+    }
+
+}
+}
